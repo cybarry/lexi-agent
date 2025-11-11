ddee63545a4a7bdfd33ed03e1aeea4bbf68a48eb
cybarry <abdulbarrshonibare@yahoo.com>
2025-11-11 08:38:26 +0100

    fix: FINAL - shipping local build artifacts to bypass broken server build

--- .mastra/output/tools.mjs
+++
@@ -1,4 +0,0 @@
-import * as tool0 from './tools/5e5610db-36c6-48ed-829a-b57035655be3.mjs';
-import * as tool1 from './tools/44e677a0-b7ea-437c-92eb-b23255257c0b.mjs';
-        
-                export const tools = [tool0, tool1]
